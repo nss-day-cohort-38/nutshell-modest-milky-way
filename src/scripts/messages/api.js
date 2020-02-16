5479c6c51f24229d4803731f5d4277c5743f45d1
castle <castlesmadeofcode@gmail.com>
2020-02-16 12:15:54 -0600

    initial file structure setup

---
+++ src/scripts/messages/api.js
@@ -0,0 +1,17 @@
+
+
+sessionStorage.setItem("activeUser", 2);
+const user = sessionStorage.getItem("activeUser");
+console.log(user);
+
+
+const API = {
+    
+    user: user,
+
+
+}
+
+
+
+export default API
