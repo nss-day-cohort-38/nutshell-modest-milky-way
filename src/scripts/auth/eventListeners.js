359fea04ec8d6e4991a095d8944a75747ed66239
castle <castlesmadeofcode@gmail.com>
2020-02-18 12:01:19 -0600

    all acceptance criteria for user registration is now being met

--- src/scripts/auth/eventListeners.js
+++ src/scripts/auth/eventListeners.js
@@ -7,44 +7,51 @@ const userDiv = document.getElementById("user__div");
 const User = {
     createRegistrationForm() {
 
-createAccount.addEventListener("click", (event) => {
-    userDiv.innerHTML += `<fieldset>username: <input type="text" id="username"></fieldset>
-    <fieldset>email: <input type="text" id="email"></fieldset>
+        createAccount.addEventListener("click", (event) => {
+            userDiv.innerHTML += `<fieldset id="field1">username: <input type="text" id="username"></fieldset>
+    <fieldset id="field2">email: <input type="text" id="email"></fieldset>
     <button id="createUserButton">Create User</button>`
 
-    this.createUserButtonListener();
+            this.createUserButtonListener();
 
-})
-},
-        createUserButtonListener() {
-            const username = document.getElementById("username");
-            const email = document.getElementById("email");
-            const createUserButton = document.getElementById("createUserButton");
-            createUserButton.addEventListener("click", (event) => {
-                const newUser= {
-                    "username": username.value,
-                    "email": email.value
-                }
-                console.log(newUser);
-                usersAPI.getAllUsers().then(users => {
-       
+        })
+    },
+    createUserButtonListener() {
+        const username = document.getElementById("username");
+        const email = document.getElementById("email");
+        const createUserButton = document.getElementById("createUserButton");
+        createUserButton.addEventListener("click", (event) => {
+            const newUser = {
+                "username": username.value,
+                "email": email.value
+            }
+            usersAPI.getAllUsers().then(users => {
+
                 let filteredUsers = users.filter(user => username.value === user.username);
-                console.log(filteredUsers);
 
-                if (filteredUsers.length === 0) { 
+                if (filteredUsers.length === 0) {
                     usersAPI.addNewUser(newUser)
-                
-
                 }
                 else {
                     window.alert("username or email is already taken")
-                  
-
                 }
-    
-                })
-})
-        }
+
+            }).then(this.setActiveUser)
+        })
+    },
+    setActiveUser() {
+        usersAPI.getAllUsers().then(users => {
+            const newUser = users.slice(-1)[0];
+            const newUserId = newUser.id;
+
+            sessionStorage.setItem("activeUser", newUserId);
+
+            userDiv.style.display = 'none'
+            const showLoggedInUser = document.getElementById("showLoggedInUser");
+            showLoggedInUser.innerHTML += `${newUser.username} is logged in`
+
+        })
     }
+}
 
 export default User
