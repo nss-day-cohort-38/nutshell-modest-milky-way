77508933fa1248ec73b4f05965f023047d463b93
castle <castlesmadeofcode@gmail.com>
2020-02-18 15:13:36 -0600

    working on login functionality

--- src/scripts/auth/eventListeners.js
+++ src/scripts/auth/eventListeners.js
@@ -10,7 +10,7 @@ const User = {
         createAccount.addEventListener("click", (event) => {
             userDiv.innerHTML += `<fieldset id="field1">username: <input type="text" id="username"></fieldset>
     <fieldset id="field2">email: <input type="text" id="email"></fieldset>
-    <button id="createUserButton">Create User</button>`
+    <button id="createUserButton">Create User</button> <button id="loginUserButton">Login</button>`
 
             this.createUserButtonListener();
 
@@ -29,6 +29,8 @@ const User = {
 
                 let filteredUsers = users.filter(user => username.value === user.username);
 
+          
+
                 if (filteredUsers.length === 0) {
 
                     usersAPI.addNewUser(newUser)
@@ -42,18 +44,29 @@ const User = {
                 else {
                     window.alert("username or email is already taken")
                 }
+            })
 
-            }).then(this.setActiveUser)
+            // }).then(this.setActiveUser(filteredUsers[0].id))
+        // }).then(this.setActiveUser())
         })
     },
     setActiveUser() {
+        usersAPI.getAllUsers().then(users => {
+
+            // let filteredUser = users.filter(user => username.value === user.username);
+            // console.log(filteredUser[0].id)
+            // const loginUserButton = document.getElementById("loginUserButton");
+            // loginUserButton.addEventListener("click", (event) => {
+            //     console.log(filteredUser[0].id);
+            //     sessionStorage.setItem("activeUser", filteredUser[0].id);
+            // });
 
 
             // userDiv.style.display = 'none';
             // const showLoggedInUser = document.getElementById("showLoggedInUser");
             // showLoggedInUser.innerHTML += `${newUser.username} is logged in`;
 
-        
+        })
     }
 }
 
