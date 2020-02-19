5a75c422931e6a49bb7b2fe1e790ed21423dc3c2
sncandiani <sofiancandiani@gmail.com>
2020-02-19 16:00:40 -0600

    developed styling on entire page

--- src/scripts/htmlMaster.js
+++ src/scripts/htmlMaster.js
@@ -3,7 +3,7 @@ const htmlMaster = {
         navBar() {
             return `
                 <header>
-                <h1 class="header-title">Nutshell</h1>
+                <a href="/src" class="header-link"><h1 class="header-title">Nutshell</h1></a>
                 <nav class="main-nav">
                     <ul>
                         <li><a href="#user__div">User</a></li>
